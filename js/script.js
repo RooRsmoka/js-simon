c668c7d18212d34f31b5bf29f6142c72ca120042
RooRsmoka <chri.disa96@gmail.com>
2021-12-23 16:59:15 +0100

    correzione esercizio

--- js/script.js
+++ js/script.js
@@ -12,33 +12,40 @@ function getRandomNum(minNum, maxNum) {
     return Math.floor(Math.random() * ((maxNum + 1) - minNum)) + minNum;
 }
 
-//
+// creo un array vuoto per i numeri che verranò generati dalla macchina.
 let numbersList = [];
+// creo l'elemento in cui verranò mostrati i numeri.
+const numberOutputHtml = document.getElementsByClassName('numbers-generated')[0];
 // Aggiungo 5 numeri randomici senza ripetizioni nell'array.
 while (numbersList.length < 5) {
-    let randomNumber = getRandomNum(1, 9999);
+    const randomNumber = getRandomNum(1, 9999);
     if (!numbersList.includes(randomNumber)) {  // Se non è incluso(!) in numbersList(array)  
         numbersList.push(randomNumber);  // aggiungi il numero all'array.
     }
 }
-//
-function getUserResponse() {
-    let userResponseList = [];
 
-    while (userResponseList.length < 5) {
-        const userResponse = parseInt(prompt('Inserisci uno dei numeri che hai visto.'));
-        if (!userResponseList.includes(userResponse)) {
-            userResponseList.push(userResponse);
-        }
-    }
-    return userResponseList  ;
+for (let i = 0; i < numbersList.length; i++) {
+    numberOutputHtml.innerHTML += numbersList[i] + ' ';
 }
 
 // Funzione waiting che aspetta 30 secondi prima di cancellare dall'html la lista di numeri.
 setTimeout(function() {
     numberOutputHtml.innerHTML = '';  // nascondo la lista di numeri sovracrivendola con una stringa vuota.
     // numberOutputHtml.className = 'hide';  // nascondo la lista di numeri aggingendo una classe e sfruttando css.
-}, 30000)
+    
+    let correctAnswers = 0;
+    let userNumbers = [];
+    
+    setTimeout(function() {
 
-const numberOutputHtml = document.getElementById('numbers-generated');
-numberOutputHtml.innerHTML = numbersList;
+        for (let i = 0; i < 5; i++) {
+            const userResponse = parseInt(prompt('Inserisci uno dei numeri che hai appena visto.'));
+
+            if (numbersList.includes(userResponse) && !userNumbers.includes(userResponse)) {
+                correctAnswers++;
+            }
+            userNumbers.push(userResponse);
+        }
+        alert('numeri indovinati: ' + correctAnswers);
+    }, 500);
+}, 29500);
